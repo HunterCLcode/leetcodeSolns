28f3f4cf4453c369174bfee13bece200b4762667
HunterCLcode <72635474+HunterCLcode@users.noreply.github.com>
2022-07-08 10:13:49 -0500

    Got rid of output variable
    Time Complex. = O(S) where S = sum of all str lengths
    Space Complex. = O(1)

--- 14-Longest-Common-Prefix.py
+++ 14-Longest-Common-Prefix.py
@@ -1,12 +1,11 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        output, minLen = 0, min(len(s) for s in strs)
+        minLen = min(len(s) for s in strs)
 
         for i in range(minLen):
             getLetter, addToOutput = strs[0][i], True
             for string in strs:
                 addToOutput = False if string[i] != getLetter else addToOutput
-            output += 1 if addToOutput else 0
             if not addToOutput:
-                break
-        return strs[0][:output]
+                return strs[0][:i]
+        return strs[0][:minLen]
