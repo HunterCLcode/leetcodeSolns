acc001178bed1540bb4f8e1f06870ae027c2ea20
HunterCLcode <72635474+HunterCLcode@users.noreply.github.com>
2022-07-16 12:17:48 -0500

    Update 36-Valid-Sudoku.py
    Much neater soln from NeetCode.io
    Time & Space Complex. = O(9^2)

--- 36-Valid-Sudoku.py
+++ 36-Valid-Sudoku.py
@@ -1,33 +1,18 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-    
-        def isValid(s):
-            row = [0] * 9
-            for i in s:
-                if i != '.':
-                    row[int(i) - 1] += 1
-                    if row[int(i) - 1] > 1:
-                        return False
-            return True      
+        # Row # / Col # / Box # --> set of integers in that section
+        rows, cols, boxes = defaultdict(set), defaultdict(set), defaultdict(set)
         
-        res = True
-        
-        for i in board:
-            res &= isValid(i)
-        
-        for i in range(0,len(board)):
-            inp = []
-            for j in range(0, len(board)):
-                inp.append(board[j][i])
-            res &= isValid(inp)
-        
-        startList = [[0,0],[3,0],[6,0],[0,3],[3,3],[6,3],[0,6],[3,6],[6,6]]
-        
-        for i,j in startList:
-            res &= isValid([board[i][j],board[i+1][j],board[i+2][j],
-            board[i][j+1],board[i+1][j+1],board[i+2][j+1],
-            board[i][j+2],board[i+1][j+2],board[i+2][j+2]])
-            
-        return res
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    continue
+                if (board[r][c] in rows[r] or
+                board[r][c] in cols[c] or
+                board[r][c] in boxes[(r//3,c//3)]):
+                    return False
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                boxes[(r//3,c//3)].add(board[r][c])
                 
-        
+        return True
