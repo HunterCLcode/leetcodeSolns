7cb3f50b6f15f1b3e0613ad4b0f01cb8d199f402
HunterCLcode <72635474+HunterCLcode@users.noreply.github.com>
2022-07-27 15:55:42 -0500

    Create 287-Find-The-Duplicate-Number.py
    Time Complex. = O(n)
    Space Complex. = O(1)
    This problem stinks!
    7/27/22

---
+++ 287-Find-The-Duplicate-Number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = fast = nums[0]
+        slow, fast = nums[slow], nums[nums[fast]]
+        while slow != fast:
+            slow, fast = nums[slow], nums[nums[fast]]
+        
+        newSlow = nums[0]
+        while slow != newSlow:
+            slow, newSlow = nums[slow], nums[newSlow]
+        return slow
