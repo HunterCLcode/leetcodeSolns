a729c1e637779dfbf64fbed67cd400ca71f6034e
HunterCLcode <72635474+HunterCLcode@users.noreply.github.com>
2023-11-22 16:52:31 -0600

    Update 121-Best-Time-to-Buy-and-Sell-Stock.py
    Fixed it to have the correct algorithm

--- 121-Best-Time-to-Buy-and-Sell-Stock.py
+++ 121-Best-Time-to-Buy-and-Sell-Stock.py
@@ -1,21 +1,7 @@
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        # Two pointers
-        l = 0
-        r = len(s)-1
-        while l < r:
-            if not self.isAlphaNumerical(s[l]):
-                l += 1
-            elif not self.isAlphaNumerical(s[r]):
-                r -= 1
-            elif s[l].lower() != s[r].lower():
-                return False
-            else:
-                l += 1
-                r -= 1
-        return True
-    
-    def isAlphaNumerical(self, c):
-        return (ord(c) >= 48 and ord(c) <= 57 or
-                ord(c) >= 65 and ord(c) <= 90 or
-                ord(c) >= 97 and ord(c) <= 122)
+    def maxProfit(self, prices: List[int]) -> int:
+        l, output = 0, 0
+        for r in range(1,len(prices)):
+            output = max(prices[r] - prices[l], output)
+            if prices[r] < prices[l]: l = r
+        return output
