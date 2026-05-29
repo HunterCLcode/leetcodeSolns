048a3790a5c3af640dc35ea9148ee258fa4d3aba
HunterCLcode <72635474+HunterCLcode@users.noreply.github.com>
2026-05-29 08:42:55 -0500

    Update 22-Generate-Parenthesis.py
    Time Complex. = O(2^n)
    Space Complex. = O(2^n)
    5/29/22

--- 22-Generate-Parenthesis.py
+++ 22-Generate-Parenthesis.py
@@ -1,14 +1,12 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        stack = [['(', n - 1, n]]
         res = []
-        
-        while len(stack):
-            paren, num, endparen = stack.pop()
-            if num < endparen:
-                stack.append([paren + ')', num, endparen - 1])
-            if num > 0:
-                stack.append([paren + '(', num - 1, endparen])
-            elif endparen == 0:
-                res.append(paren)
+
+        def DFS(path, parenLeft, parenClose):
+            if not parenLeft and not parenClose: res.append(path)
+
+            if parenLeft: DFS(path + '(', parenLeft - 1, parenClose + 1)
+            if parenClose: DFS(path + ')', parenLeft, parenClose - 1)
+
+        DFS("", n, 0)
         return res
